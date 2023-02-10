7c96d5040073ea691daa23dcdc386f3777596129
yashs <yashs@192.168.2.16>
2023-02-10 09:31:23 -0500

    initialize project

---
+++ first-web-application/src/main/java/com/webapplication/LoginServlet.java
@@ -0,0 +1,18 @@
+package com.webapplication;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response )throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		}
+}
