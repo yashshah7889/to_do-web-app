1e0bbf038561fcc8d52a0e785961d9ce509e57c4
yashs <yashs@192.168.2.16>
2023-02-11 09:00:09 -0500

    todos added

---
+++ first-web-application/src/main/java/com/webapplication/todo/TodoController.java
@@ -0,0 +1,25 @@
+package com.webapplication.todo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.webapplication.login.UserValidation;
+
+@Controller
+public class TodoController {
+	
+	@Autowired
+	TodoService service;
+	
+	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
+	public String showLoginPage(ModelMap model) {
+		model.addAttribute("todos",service.retrieveTodos("in28Minutes"));
+		return "list-todos";
+	}
+	
+}
---
+++ first-web-application/src/main/java/com/webapplication/todo/TodoService.java
@@ -0,0 +1,44 @@
+package com.webapplication.todo;
+
+import java.util.*;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+	private static List<Todo> todos = new ArrayList<Todo>();
+	private static int todoCount = 3;
+
+	static {
+		todos.add(new Todo(1, "in28Minutes", "Learn Spring MVC", new Date(),
+				false));
+		todos.add(new Todo(2, "in28Minutes", "Learn Struts", new Date(), false));
+		todos.add(new Todo(3, "in28Minutes", "Learn Hibernate", new Date(),
+				false));
+	}
+	
+	public void addTodo(String name, String desc, Date targetDate, boolean isDone) {
+		todos.add(new Todo(++todoCount, name, desc, targetDate, isDone));
+	}
+
+	public void deleteTodo(int id) {
+		Iterator<Todo> iterator = todos.iterator();
+		while (iterator.hasNext()) {
+			Todo todo = iterator.next();
+			if (todo.getId() == id) {
+				iterator.remove();
+			}
+		}
+	}
+	
+	public List<Todo> retrieveTodos(String user) {
+		List<Todo> filteredTodos = new ArrayList<Todo>();
+		for (Todo todo : todos) {
+			if (todo.getUser().equals(user))
+				filteredTodos.add(todo);
+		}
+		return filteredTodos;
+	}
+
+	
+}
